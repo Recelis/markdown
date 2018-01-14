4b5ae428d551a1a3e01b0a983fa397ab07a4e6d7
JackyRecelis <jackymlui@live.com>
2018-01-14 19:03:12 +1000

    github link

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
 import Converter from './components/Converter'
-
+import github from "./img/github.png";
 
 
 class App extends Component {
+  openWindow(){
+    window.open("https://github.com/Recelis/CamperLeaderBoard",'_blank');
+  }
+
   render() {
     return (
       <div>
         <h1 className="title">Markdown Previewer</h1>
         <Converter
         />
-        <p className="copyright">Created by Jacky Lui 2017</p>
+        <p className="copyright">Created by Jacky Lui 2017
+        <span><button onClick = {()=>this.openWindow()}className = "github"><img className = "github" src = {github}></img></button></span>
+        </p>
       </div>
     );
   }
